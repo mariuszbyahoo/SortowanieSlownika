5f77295adf5f59b96ad50c5686138d1c707916e7
mariusz.budzisz <mariusz.budzisz@morlab.onmicrosoft.com>
2021-12-05 18:36:18 +0100

    Pomieszanie słów bez kolejności

--- SortowanieSlownika.DAL/MockDictionaryRepo.cs
+++ SortowanieSlownika.DAL/MockDictionaryRepo.cs
@@ -11,27 +11,27 @@ namespace SortowanieSlownika.DAL
         public IDictionary<string, string> GetAll()
         {
             var result = new Dictionary<string, string>();
-            result.Add("Alicja", "Alojzy");
             result.Add("Bernadetta", "Błażej");
-            result.Add("Cecylia", "Cezary");
+            result.Add("Teodora", "Tadeusz");
+            result.Add("Alicja", "Alojzy");
+            result.Add("Wiktoria", "Wikariusz");
             result.Add("Danuta", "Dariusz");
             result.Add("Ewa", "Eustachy");
+            result.Add("Zenon", "Zaułek");
             result.Add("Frombork", "Francja");
-            result.Add("Genowefa", "Gienek");
             result.Add("Halina", "Heniek");
             result.Add("Izabela", "Irek");
-            result.Add("Jolanta", "Janusz");
             result.Add("Klaudia", "Kamil");
             result.Add("Las", "Leśny");
-            result.Add("Monika", "Mariusz");
             result.Add("Natalia", "Norbert");
             result.Add("Ola", "Olaf");
-            result.Add("Patrycja", "Patrycjusz");
             result.Add("Renata", "Robert");
-            result.Add("Teodora", "Tadeusz");
             result.Add("Urszula", "Ul");
-            result.Add("Wiktoria", "Wikariusz");
-            result.Add("Zenon", "Zaułek");
+            result.Add("Patrycja", "Patrycjusz");
+            result.Add("Cecylia", "Cezary");
+            result.Add("Genowefa", "Gienek");
+            result.Add("Jolanta", "Janusz");
+            result.Add("Monika", "Mariusz");
             return result;
         }
     }
